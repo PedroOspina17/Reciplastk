77bea9515362170e9a7fcc36f2e365f25512b396
Pedro-Ospina <pedro.ospina@coxautoinc.com>
2024-03-08 08:47:12 -0500

    Create front end and login functionality

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/security/login/login.component';
+import { RegisterComponent } from './components/security/register/register.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { AboutusComponent } from './components/general/aboutus/aboutus.component';
+import { WelcomeComponent } from './components/general/welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'admin',
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'roles', redirectTo: '/dashboard' },
+    ],
+  },
+  { path: '**', redirectTo: '/welcome', pathMatch: 'full' },
+];
--- Front/src/app/components/security/login/login.component.ts
+++ Front/src/app/components/security/login/login.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
+import { LoaderComponent } from '../../shared/loader/loader.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { RouterLink, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { SecurityService } from '../../../services/security.service';
+import { LoginModel } from '../../../models/LoginModel';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, LoaderComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  isLoading: boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private toastr: ToastrService,
+    private router: Router,
+    private securityService: SecurityService
+  ) {
+    this.loginForm = fb.group({
+      email: ['', [Validators.required]],
+      password: ['', Validators.required],
+    });
+  }
+
+  Save(): void {
+    
+    const loginData: LoginModel = {
+      userName: this.loginForm.value.email,
+      password: this.loginForm.value.password
+    };
+
+    this.isLoading = true;
+
+    this.securityService.LogIn(loginData).subscribe(result => {
+      // Here i can place any code i want.      
+      console.log("Login result", result);
+      if(result.wasSuccessful == true){
+        this.toastr.success(`Bienvenido, ${result.data.name} ${result.data.lastname} al sistema`, 'Bienvenido!');
+        this.router.navigate(['/admin/dashboard']);
+      }else{
+        console.log("informacion incorrecta");
+        this.toastr.error('El usuario o contraseña ingresado es incorrecto.', 'Invalid credentials!');
+        
+      }
+    });
+    
+    
+    this.isLoading = false;
+  }
 
 }
