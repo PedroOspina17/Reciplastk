2b14ab9067bc4beccc8dab989af0fb33d9a85564
David Alejandro Ospina <davidossa0110@gmail.com>
2024-05-09 16:20:58 -0500

    creacion de comparacion del nit con la bd

---
+++ Back/Reciplastk.Gateway/Services/CustumerService.cs
@@ -0,0 +1,40 @@
+﻿using Reciplastk.Gateway.DataAccess;
+using System.Linq;
+
+namespace Reciplastk.Gateway.Services
+{
+    public class CustumerService
+    {
+        ReciplastkContext db = new ReciplastkContext();
+
+        public void SaveNit(string nit)
+        {
+            var customer = db.Customers.Where(x => x.Nit == nit );
+        }
+        public void SaveName()
+        {
+
+        }
+        public void SaveLastName()
+        {
+
+        }
+        public void SaveAdress()
+        {
+
+        }
+        public void SaveCelphone()
+        {
+
+        }
+        public void SaveClientSince()
+        {
+
+        }
+        public void NeedsPickUp()
+        {
+
+        }
+       
+    }
+}
