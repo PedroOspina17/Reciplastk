693af9c957b5ee383da87c5285803460b7fe141c
David Alejandro Ospina <davidossa0110@gmail.com>
2024-11-20 08:20:21 -0500

    Cambios de posicion

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -36,6 +36,7 @@ export const routes: Routes = [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'PaymentReceiptComponent', component: PaymentReceiptComponent },
       { path: 'PaymentReceiptComponent/:id', component: PaymentReceiptComponent },
+      { path: 'products', component: ListProductsComponent },
       { path: 'addProduct', component: AddEditProductsComponent },
       { path: 'editProduct/:id', component: AddEditProductsComponent },
       { path: 'RemainigComponent', component: RemainigComponent },
@@ -58,7 +59,6 @@ export const routes: Routes = [
       { path: 'addshipmenttype', component: AddEditShipmentTypeComponent },
       { path: 'editshipmenttype/:id', component: AddEditShipmentTypeComponent },
       { path: 'roles', redirectTo: '/dashboard' },
-      { path: 'products', component: ListProductsComponent },
     ]
   },
   {
